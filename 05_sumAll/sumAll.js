8a39bcea24b869120f5c6c1ea4e1f45729576d01
Nyam-Erdene N <Nyamka523@gmail.com>
2022-06-20 15:50:09 +0800

    fixed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let sumTotal = 0;
+if (args[0] < 0 || args[1] < 0) {
+    return "ERROR";
+     }
+if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])) {
+    return "ERROR";
+}
+    
+else if (args[0] < args[1]) {
+    for (let i = args[0]; i <= args[1]; i++){
+        sumTotal = sumTotal + i;
+    }
+}
+else if (args[0] > args[1]) {
+    for (let i = args[1]; i <=args[0]; i++){
+        sumTotal = sumTotal + i;
+    }
+}
 
+return sumTotal;
 };
 
 // Do not edit below this line
