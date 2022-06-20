581e703081c10f1ebf690e9823022606303a1e8d
Nyam-Erdene N <Nyamka523@gmail.com>
2022-06-20 14:04:04 +0800

    Fixed first 3 exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    //split() to return a new array
+    var splitString = str.split("");
+    //reverse() to reverse the new created array
+    var reverseArray = splitString.reverse();
+    //join() to join all the elements in the array into a string
+    var joinArray = reverseArray.join("");
 
+    return joinArray;
 };
 
 // Do not edit below this line
