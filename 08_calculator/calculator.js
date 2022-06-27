1dd179f0d8e9e5a03cacc7825cd03cab91e47d35
Nyam-Erdene N <Nyamka523@gmail.com>
2022-06-27 14:50:46 +0800

    Yeah got covered everything so far

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+  let adds = a + b;
+  return adds;
+};
+
+
+const subtract = function(a, b) {
+  let sub = a - b;
+  return sub;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 
 };
 
-const power = function() {
-	
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const factorial = function() {
+const power = function(base, power) {
+	return Math.pow(base, power);
+};
+
+const factorial = function(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    for (let i = 1;  i <= number ; i++) {
+      number = number * i;
+    }
+    return number;
+  }
 	
 };
 
