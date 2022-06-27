ee7e54a9a2b9af02e618a2632857e1ca97070679
Nyam-Erdene N <Nyamka523@gmail.com>
2022-06-27 16:08:43 +0800

    done 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    var re = /[\W_]/g;
+    var lowRegStr = text.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
